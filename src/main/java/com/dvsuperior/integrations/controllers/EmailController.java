e9bba6354e47db1330a071b3582027ba67649a99
FelipeGabrill <felipegabrielodss@gmail.com>
2025-02-10 00:43:49 -0300

    Enviando primeiro email com SendGrid

--- src/main/java/com/dvsuperior/integrations/controllers/EmailController.java
+++ src/main/java/com/dvsuperior/integrations/controllers/EmailController.java
@@ -1,5 +1,6 @@
 package com.dvsuperior.integrations.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,13 +8,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dvsuperior.integrations.dto.EmailDTO;
+import com.dvsuperior.integrations.service.EmailService;
 
 @RestController
 @RequestMapping(value = "/emails")
 public class EmailController {
+	
+	@Autowired
+	private EmailService service;
 
 	@PostMapping
 	public ResponseEntity<Void> send(@RequestBody EmailDTO dto) {
+		service.sendEmail(dto);
 		return ResponseEntity.noContent().build();
 	}
 }
